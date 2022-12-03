b76fc6812fcb60523ca8ccab7d8a7c938e2bbc39
Scifi Mackbook_Air <sifisoldhlamini@outlook.com>
2022-12-04 02:56:16 +0800

    removed ToucableOpacity and used button instead. Refactored prop ppaparameter and used destructuring just to use navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.style}>Student Buddy</Text>
       {/*A Button is a basic component that should render nicely on any platform. On iOS, the button uses a system color, whereas on Android, the button will use the theme color. */}
       <Button
-        onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigation.navigate("Components")}
         title="Go to Components Demo"
       />
-      <TouchableOpacity onPress={()=> props.navigation.navigate("List") }>
+      <Button 
+        onPress={()=> navigation.navigate("List")}
+        title="Go to List Demo"
+      />
       {/*TouchableOpacity is a button that is more customizable*/}
+      {/* <TouchableOpacity onPress={()=> props.navigation.navigate("List") }>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 };
