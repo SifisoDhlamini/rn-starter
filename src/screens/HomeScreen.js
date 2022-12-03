bcdaa0dd2bdba405f2cb3ea25c609eb4addee801
Scifi Mackbook_Air <sifisoldhlamini@outlook.com>
2022-12-04 02:44:01 +0800

    added navigation on buttons by passing props on components created and using navigation.navigate function, passing it the function on the onPress() function

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.style}>Student Buddy</Text>
+      {/*A Button is a basic component that should render nicely on any platform. On iOS, the button uses a system color, whereas on Android, the button will use the theme color. */}
       <Button
-      {/* A Button is a basic component that should render nicely on any platform. On iOS, the button uses a system color, whereas on Android, the button will use the theme color. */}
-        onPress={() => console.log("Button pressed")}
+        onPress={() => props.navigation.navigate("Components")}
         title="Go to Components Demo"
       />
-      <TouchableOpacity onPress={()=> console.log("List Pressed") }>
+      <TouchableOpacity onPress={()=> props.navigation.navigate("List") }>
       {/*TouchableOpacity is a button that is more customizable*/}
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
