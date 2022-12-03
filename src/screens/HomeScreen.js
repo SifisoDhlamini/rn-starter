ac74465d364be4319cbff30ebaee5d5ef6801914
Scifi Mackbook_Air <sifisoldhlamini@outlook.com>
2022-12-04 02:14:32 +0800

    Added comments

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,10 +6,12 @@ const HomeScreen = () => {
     <View>
       <Text style={styles.style}>Student Buddy</Text>
       <Button
+      {/* A Button is a basic component that should render nicely on any platform. On iOS, the button uses a system color, whereas on Android, the button will use the theme color. */}
         onPress={() => console.log("Button pressed")}
         title="Go to Components Demo"
       />
       <TouchableOpacity onPress={()=> console.log("List Pressed") }>
+      {/*TouchableOpacity is a button that is more customizable*/}
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
