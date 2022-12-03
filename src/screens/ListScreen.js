53fab8cdb8eb5cdac8194c220acc60e64db2a6c8
Scifi Mackbook_Air <sifisoldhlamini@outlook.com>
2022-12-04 00:44:47 +0800

    Created ListScreen and added FlatList

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Text, StyleSheet, View, FlatList } from "react-native";
+
+const ListScreen = () => {
+    const friends = [
+        { name: "Friend #1", age: 20 },
+        { name: "Friend #2", age: 45 },
+        { name: "Friend #3", age: 32 },
+        { name: "Friend #4", age: 27 },
+        { name: "Friend #5", age: 53 },
+        { name: "Friend #6", age: 30 },
+        { name: "Friend #7", age: 21 },
+        { name: "Friend #8", age: 19 },
+        { name: "Friend #9", age: 18 },
+    ];
+    
+    
+    return (
+        <FlatList 
+        data={friends} 
+        renderItem={({ item }) => {
+            return <Text>{item.name}</Text>;
+        }}
+        />
+    );
+
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
+   
--- src/screens/componentsScreen.js
+++ src/screens/componentsScreen.js
@@ -5,18 +5,26 @@ const ComponentsScreen = () => {
     const name = "ScifiTheDev";
     return (
         <View>
-            <Text style = {styles.textStyle}>Getting started with react Native!</Text>
-            <Text style={styles.subHeaderStyle}>Hi my name is {name}</Text>
+            <Text style = {styles.textStyle}>Getting started with React Native!</Text>
+            <Text style={styles.subHeaderStyle}>{name} going mobile</Text>
+            <Text style={styles.subsubHeaderStyle}>......humble beginings</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     textStyle:{
-        fontSize: 45
+        fontSize: 45,
+        textAlign: 'center'
     },
     subHeaderStyle:{
-        fontSize: 20
+        fontSize: 30,
+        //textAlign: 'center'
+    }
+    ,
+    subsubHeaderStyle:{
+        fontSize: 20,
+        //textAlign: 'center'
     }
 });
 
