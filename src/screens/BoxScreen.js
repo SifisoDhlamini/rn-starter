d21b9f3aa28ba2b3cd974d8a7270d2443050c1fa
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2023-05-10 14:12:06 +0800

    Layout exercise complete

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,31 +1,50 @@
 import React from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  LogBox,
-  FlatList,
-  FlexStyle,
-} from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+import { color } from "react-native-reanimated";
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+    <View style={styles.mainViewStyle}>
+      <View style={styles.childViewOneStyle} />
+      <View style={styles.childViewTwoStyle} />
+      <View style={styles.childViewThreeStyle} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  mainViewStyle: {
     borderWidth: 3,
     borderColor: "black",
+    //alignItems: "center",
+    flexDirection: "row",
+    height: 400,
+    justifyContent: "space-between",
   },
-  textStyle: {
-    borderWidth: 1,
+  childViewOneStyle: {
+    borderWidth: 3,
     borderColor: "red",
-    marginVertical: 20,
-    marginHorizontal: 20,
+    height: 100,
+    width: 100,
+    backgroundColor: "rgba(255, 0, 0, 0.5)", // red color with 50% opacity
+  },
+  childViewTwoStyle: {
+    borderWidth: 3,
+    borderColor: "green",
+    //flex: 3,
+    height: 100,
+    width: 100,
+    backgroundColor: "rgba(0, 255, 0, 0.5)", // red color with 50% opacity
+    //marginTop: 100,
+    top: 100,
+  },
+  childViewThreeStyle: {
+    borderWidth: 3,
+    borderColor: "blue",
+    //flex: 3,
+    height: 100,
+    width: 100,
+    backgroundColor: "rgba(0, 0, 255, 0.5)", // red color with 50% opacity
   },
 });
 
